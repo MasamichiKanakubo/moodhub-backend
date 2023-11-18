bdaca8515d9c35b51e2ce022095a0a85109ea051
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2023-11-18 21:20:49 +0900

    feature: make new schema updating user_name

--- test.py
+++ test.py
@@ -10,16 +10,24 @@ from strawberry.asgi import GraphQL
 from fastapi.middleware.cors import CORSMiddleware
 import random
 from collections import defaultdict
+import redis
 
 
 load_dotenv()
+
+redis_client = redis.Redis(
+    host=os.getenv("REDIS_HOST"),
+    port=os.getenv("REDIS_PORT"),
+    password=os.getenv("REDIS_PASSWORD"),
+    ssl=True,
+)
 
 client = MongoClient(os.environ["MONGO_URL"])
 db = client["RoomDB"]
 
 
-client_id = os.getenv('CLIENT_ID') 
-client_secret = os.getenv('CLIENT_SECRET')
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyClientCredentials(
@@ -27,11 +35,12 @@ sp = spotipy.Spotify(
     )
 )
 
-results = sp.search(q="ひげだん", limit=10, market='JP', type="track")
-for track in results['tracks']['items']:
-    name = track['name']
-    track_id = track['id']
+results = sp.search(q="ひげだん", limit=5, market="JP", type="track")
+for track in results["tracks"]["items"]:
+    name = track["name"]
+    track_id = track["id"]
+    print(track_id, name)
     track_info = sp.audio_features(track_id)
-    bpm = track_info[0]['tempo']
-    print(f'{name} : {bpm}')
-
+    bpm = track_info[0]["tempo"]
+    print(f"{name} : {bpm}")
+    
